1ac918c61ccf4277a8d06d42c9819277d6a5644d
ricardonava <rnavarrete@quaxar.com>
2020-10-19 10:33:56 -0500

    Apis for AirBnB Clone

---
+++ app/models/user.rb
@@ -0,0 +1,81 @@
+class User < ApplicationRecord
+  before_create :generate_authentication_token
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable, :omniauthable
+
+  validates :fullname, presence: true, length: {maximum: 50}
+  
+  #One user can have many rooms
+  has_many :rooms
+  has_many :reservations
+
+  #Reviews for reservations
+  has_many :guest_reviews, class_name: "GuestReview", foreign_key: "guest_id"
+  has_many :host_reviews, class_name: "HostReview", foreign_key: "host_id"
+  has_many :notifications
+
+  has_one :setting # An user can have one setting
+  after_create :add_setting # After create one user, will have a setting
+
+  def add_setting
+    Setting.create(user: self, enable_sms: true, enable_email: true)
+  end
+
+  #This method tries to find an existing user by the provider and uid fields. If no user is found, 
+  #a new one is created with a random password and some extra information
+  def self.from_omniauth(auth)
+    user = User.where(email: auth.info.email).first
+
+    if user
+      return user
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.fullname = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+      user.uid = auth.uid
+      user.provider = auth.provider
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      user.skip_confirmation!
+      end
+    end
+  end
+
+  #To sms with the gem twilio
+  def generate_pin
+    #Random pin
+    self.pin = SecureRandom.hex(2)
+    self.phone_verified = false
+    save
+  end
+
+  def send_pin
+    # set up a client to talk to the Twilio REST API
+    @client = Twilio::REST::Client.new
+    #Send an SMS
+    @client.messages.create(
+      from: '+14252742501',
+      to: self.phone_number,
+      body: "Your pin is #{self.pin}"
+    )
+  end
+
+  def verify_pin(entered_pin)
+    update(phone_verified: true) if self.pin == entered_pin
+  end
+
+  #Stripe
+  def is_active_host
+    !self.merchant_id.blank?
+  end
+
+  def generate_authentication_token
+    begin
+      self.access_token = Devise.friendly_token
+    end while self.class.exists?(access_token: access_token)
+  end
+end
---
+++ config/initializers/koala.rb
@@ -0,0 +1,7 @@
+Koala.configure do |config|
+    
+    config.app_id = '393880314902428'
+    config.app_secret = '72fb88e71201a1a19fac999aba0c25a5'
+    # See Koala::Configuration for more options, including details on how to send requests through
+    # your own proxy servers.
+end
